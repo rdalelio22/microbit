d58058155e841c0727a7de345caf2b98b8bb1ba9
rdalelio22 <94003317+rdalelio22@users.noreply.github.com>
2021-11-18 13:03:23 -0500

    Add files via upload

---
+++ Micro animation button.py
@@ -0,0 +1,81 @@
+# Add your Python code here. E.g.
+from microbit import *
+
+def anim():             
+    display.show(Image('98765:'
+                   '56789:'
+                   '44444:'
+                   '56789:'
+                   '98765:'))
+    sleep(750)
+    display.show(Image('56789:'
+                   '98765:'
+                   '44444:'
+                   '98765:'
+                   '56789:'))
+    sleep(750)
+    display.show(Image('95459:'
+                   '86468:'
+                   '77477:'
+                   '68486:'
+                   '59495:'))
+    sleep(750)
+    display.show(Image('59495:'
+                   '68486:'
+                   '77477:'
+                   '86468:'
+                   '95459:'))
+    sleep(750)
+    display.show(Image('95459:'
+                   '59495:'
+                   '45954:'
+                   '59495:'
+                   '95459:'))
+    sleep(750)
+    
+def backwardanim():
+    display.show(Image('59495:'
+                   '68486:'
+                   '77477:'
+                   '86468:'
+                   '95459:'))
+    sleep(750)
+    display.show(Image('95459:'
+                   '86468:'
+                   '77477:'
+                   '68486:'
+                   '59495:'))
+    sleep(750)
+    display.show(Image('56789:'
+                   '98765:'
+                   '44444:'
+                   '98765:'
+                   '56789:'))
+    sleep(750)
+    display.show(Image('98765:'
+                   '56789:'
+                   '44444:'
+                   '56789:'
+                   '98765:'))
+    sleep(750)
+    display.show(Image('95459:'
+                   '59495:'
+                   '45954:'
+                   '59495:'
+                   '95459:'))
+    sleep(750)
+    
+    
+    
+while True:
+    if button_a.is_pressed and button_b.is_pressed():
+        break   
+    
+    if button_a.is_pressed():
+        anim()
+    
+    if button_b.is_pressed():
+        backwardanim()
+    
+    
+display.scroll("bye")
